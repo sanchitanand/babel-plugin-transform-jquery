af54576546d01f4f05bfffd85f19cda3504e32d6
Sanchit Anand <sanxchit@gmail.com>
2018-04-18 01:19:23 -0400

    Renaming, added chai testing

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
-import assert from 'assert';
+import {assert} from 'chai';
 import babelPluginTransformjQuery from '../src';
 import * as babel from 'babel-core';
 
 describe("Basics", function ()
 {
-    it("Can import library", function ()
+    it("Can import plugin", function ()
     {
         assert(typeof babelPluginTransformjQuery === "function");
     });
