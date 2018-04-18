27fc2ab7a1777809cf96284c13c79f4034feccbb
Sanchit Anand <sanxchit@gmail.com>
2018-04-18 01:13:13 -0400

    Added chai, created first test

--- test/index.js
+++ test/index.js
@@ -2,4 +2,12 @@ import assert from 'assert';
 import babelPluginTransformjQuery from '../src';
 import * as babel from 'babel-core';
 
-console.log(typeof  babelPluginTransformjQuery);
+describe("Basics", function ()
+{
+    it("Can import library", function ()
+    {
+        assert(typeof babelPluginTransformjQuery === "function");
+    });
+
+});
+
