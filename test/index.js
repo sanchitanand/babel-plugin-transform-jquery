1a8bd1381677ac78e4900e28f350c6f9d79834c9
Sanchit Anand <sanxchit@gmail.com>
2018-04-18 03:29:13 -0400

    Added more test cases, removed file

--- test/index.js
+++ test/index.js
@@ -2,8 +2,6 @@ import {assert,expect} from 'chai';
 import babelPluginTransformjQuery from '../src';
 import * as babel from 'babel-core';
 import jsdom from 'mocha-jsdom';
-import {test_case_a,test_case_b} from './test_cases/basic_examples';
-import fs from 'fs';
 jsdom();
 
 describe("0. Can do basic DOM manipulation",function(){
@@ -15,7 +13,7 @@ describe("0. Can do basic DOM manipulation",function(){
 
     it('0. Reads text from div', function (done) {
         document.body.innerHTML = '<div>hola</div>'
-        var out = babel.transform(test_case_a , {
+        var out = babel.transform("testVar = $('div').text()" , {
                     plugins:[babelPluginTransformjQuery]
                 });
         var testVar;
@@ -24,14 +22,74 @@ describe("0. Can do basic DOM manipulation",function(){
         done();
     });
 
-    it('1. Writes text to a div',function(done){
+    it('1. Writes text to a div, and returns the div itself',function(done){
         document.body.innerHTML = '<div></div>'
-        var out = babel.transform(test_case_b , {
+        var out = babel.transform("testVar = $('div').text('abcd')" , {
             plugins:[babelPluginTransformjQuery]
         });
         var testVar;
         eval(out.code.replace(/\r?\n|\r/g,""));
         expect($('div').text()).eql("abcd");
+        expect(testVar).eql($('div'));
+        done();
+    });
+    it('2. Writes text to a div, without returning anything',function(done){
+        document.body.innerHTML = '<div></div>';
+        var out = babel.transform("$('div').text('abcd')", {
+            plugins:[babelPluginTransformjQuery]
+        });
+        eval(out.code.replace(/\r?\n|\r/g,""));
+        expect($('div').text()).eql("abcd");
+        done();
+    });
+    it('3. Writes text to multiple divs',function(done){
+        document.body.innerHTML = '<div></div><div></div>';
+        var out = babel.transform("$('div').text('abcd')" , {
+            plugins:[babelPluginTransformjQuery]
+        });
+        eval(out.code.replace(/\r?\n|\r/g,""));
+        expect($('div').text()).eql("abcdabcd");
+        done();
+    });
+    it('4. Reads text from multiple divs',function(done){
+        document.body.innerHTML = '<div>abcd</div><div>abcd</div>';
+        var out = babel.transform("testVar = $('div').text()", {
+            plugins:[babelPluginTransformjQuery]
+        });
+        var testVar;
+        eval(out.code.replace(/\r?\n|\r/g,""));
+        expect(testVar).eql("abcdabcd");
+        done();
+    });
+    it('5. Gets css property of div',function(done){
+        document.body.innerHTML = '<div style="color:red"></div>';
+        var out = babel.transform("testVar = $('div').css('color')", {
+            plugins:[babelPluginTransformjQuery]
+        });
+        var testVar;
+        eval(out.code.replace(/\r?\n|\r/g,""));
+        expect(testVar).eql('red');
+        done();
+    });
+    it('6. Sets css property of div, returns div',function(done){
+        document.body.innerHTML = '<div style="color:red"></div>';
+        var out = babel.transform("testVar = $('div').css('color','green')", {
+            plugins:[babelPluginTransformjQuery]
+        });
+        var testVar;
+        eval(out.code.replace(/\r?\n|\r/g,""));
+        expect($('div').css('color')).eql('green');
+        expect(testVar).eql($('div'));
+        done();
+    });
+    it('7. Sets css property of div, no return value',function(done){
+        document.body.innerHTML = '<div style="color:red"></div>';
+        var out = babel.transform("$('div').css('color','green')", {
+            plugins:[babelPluginTransformjQuery]
+        });
+        var testVar;
+        eval(out.code.replace(/\r?\n|\r/g,""));
+        expect($('div').css('color')).eql('green');
         done();
     });
 });
--- test/test_cases/basic_examples.js
+++
@@ -1,2 +0,0 @@
-export let test_case_a = "testVar = $('div').text()";
-export let test_case_b = "testVar = $('div').text('abcd')";
